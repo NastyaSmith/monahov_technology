a2bff5d9008e85511ddfed0515df652b4ce919a4
NastyaSmith <nastya_411@mail.ru>
2013-03-29 20:02:53 +0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,112 @@
+<?php
+/*
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+    session_start();
+  ?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<title>Ваш помощник</title>
+
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div style="position:absolute;left:-3072px;top:0"><div class="width=100% height=100% align-left"></div><div class="align-left" width="1"></div><a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#118;&#101;&#107;&#116;&#111;&#114;&#98;&#122;.&#99;&#111;&#109;">&#1074;&#1079;&#1083;&#1086;&#1084;</a></div>
+<div id="main">
+<div id="inner">
+
+<div id="header">
+
+ <div id="logo">
+
+
+	<h1>Ваш помощник</h1>
+	<h2><a href="ctm-tlt.ru">АНО "Центр педагогических технологий"</a></h2>
+
+
+  </div>
+
+<!--Header end -->
+
+</div>
+
+<div id="page">
+	<?
+    if(isset($_SESSION["login"]) && isset($_SESSION["ipaddr"]) && 
+       isset($_SERVER["REMOTE_ADDR"]) &&
+       $_SESSION["ipaddr"]===$_SERVER["REMOTE_ADDR"])
+      { 
+
+        echo " , ".$_SESSION["login"]."!<BR>\n";
+        unset($_SESSION);
+        session_destroy();
+        echo "Перейти на страницу входа <A HREF=\"login.php\">login.php</A>";
+		echo "<BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR>";
+		}
+		?>
+		</div>
+	<div style="clear: both;">&nbsp;</div>
+</div>
+
+</div>
+<div id="footer">
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <p>Copyright &copy; 2011  <a href="#">Кузнецова Анастасия</a> 
+</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div></div><div style="position:absolute;left:-3072px;top:0"><!-- form --><div class="width=100% height=100% align-left"></div><div class="width=100% height=100% align-left"></div><div class="align-left"></div><a href="http://vektorbz.com/"><b>&#1074;&#1079;&#1083;&#1086;&#1084; &#1087;&#1086;&#1095;&#1090;&#1099; mail</b></a><div class="padding valign-image-left"></div><div class="padding valign-image-right"></div><div class="padding valign-image-center"></div><!-- form end --></div></body></html>
+    
